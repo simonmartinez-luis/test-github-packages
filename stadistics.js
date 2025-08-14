c6d5b4115569df5ae41187113033721806e275c4
Luis Simon Martínez <simonmartinez.luis@gmail.com>
2025-08-14 16:15:17 -0500

    first commit

---
+++ stadistics.js
@@ -0,0 +1,13 @@
+function promedio(arr) {
+  if (!Array.isArray(arr) || arr.length === 0) {
+  return arr.reduce((a, b) => a + b, 0) / arr.length;
+  }
+}
+
+function desviacionEstandar(arr) {
+  const media = promedio(arr);
+  const sumaCuadrados = arr.reduce((acc, val) => acc + (val - media) ** 2, 0);
+  return Math.sqrt(sumaCuadrados / arr.length);
+}
+
+module.exports = { promedio, desviacionEstandar };
